6d7dd479e729f418b3fb09ab5173b7feb384aa1a
Lowkey.Lowky <139440439+huyentrang04012003@users.noreply.github.com>
2025-07-03 14:54:03 +0700

    wording

--- 3Nest/src/utils/data.js
+++ 3Nest/src/utils/data.js
@@ -9,6 +9,7 @@ import {
   Folders,
   Settings,
   Layers,
+  Building2,
 } from 'lucide-react';
 export const SIDE_MENU_DATA = [
   {
@@ -35,13 +36,14 @@ export const SIDE_MENU_DATA = [
     resourceType: 'order',
     ariaLabel: 'Navigate to Quote',
   },
-  {
-    id: '02',
-    label: 'Items',
-    icon: Package,
-    path: '/products',
-    resourceType: 'product',
-    ariaLabel: 'Navigate to products',
+  
+   {
+    id: '07',
+    label: 'Companies',
+    icon: Building2,
+    path: '/types',
+    resourceType: 'type',
+    ariaLabel: 'Navigate to types',
   },
   {
     id: '03',
@@ -52,36 +54,20 @@ export const SIDE_MENU_DATA = [
     ariaLabel: 'Navigate to categories',
   },
   {
+    id: '02',
+    label: 'Items',
+    icon: Package,
+    path: '/products',
+    resourceType: 'product',
+    ariaLabel: 'Navigate to products',
+  },
+  {
     id: '05',
     label: 'Users',
     icon: Users,
     path: '/users',
     resourceType: 'user',
     ariaLabel: 'Navigate to users',
-  },
-  {
-    id: '06',
-    label: 'Reports',
-    icon: BarChart2,
-    path: '/reports',
-    resourceType: 'report',
-    ariaLabel: 'Navigate to reports',
-  },
-  {
-    id: '07',
-    label: 'Types',
-    icon: Layers,
-    path: '/types',
-    resourceType: 'type',
-    ariaLabel: 'Navigate to types',
-  },
-  {
-    id: '09',
-    label: 'Per Type',
-    //icon: ShoppingCart,
-    path: '/pertype',
-    resourceType: 'per_type',
-    ariaLabel: 'Navigate to per type reports',
   },
   {
     id: '10',
@@ -91,6 +77,24 @@ export const SIDE_MENU_DATA = [
     resourceType: 'role',
     ariaLabel: 'Navigate to roles',
   },
+  {
+    id: '06',
+    label: 'Reports',
+    icon: BarChart2,
+    path: '/reports',
+    resourceType: 'report',
+    ariaLabel: 'Navigate to reports',
+  },
+ 
+  {
+    id: '09',
+    label: 'Per Type',
+    //icon: ShoppingCart,
+    path: '/pertype',
+    resourceType: 'per_type',
+    ariaLabel: 'Navigate to per type reports',
+  },
+  
   {
     id: '11',
     // label: 'Act Log',
